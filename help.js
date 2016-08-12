497c218f7c7a905e88b102dc92a8205b15390d60
monicafp <monikfornaris@gmail.com>
2016-08-11 20:56:03 -0400

    Add help file

---
+++ help.js
@@ -0,0 +1,3 @@
+var colors = require('colors');
+
+console.log("This is a BAC application which will help you determine if you are able to drive or not after drinking Medalla. We will ask you for your name, gender, how many beers you have had and how many hours it has been since your last beer. Run the command bac to answer the previous questions and know if you should drive or not.".yellow);
