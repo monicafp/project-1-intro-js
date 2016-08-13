87c3a5c2556eff648582ffe248a5bcaf253dc4fa
monicafp <monikfornaris@gmail.com>
2016-08-13 15:27:50 -0400

    Edit help message

--- help.js
+++ help.js
@@ -1,3 +1,3 @@
 var colors = require('colors');
 
-console.log("This is a BAC application which will help you determine if you are able to drive or not after drinking Medalla. We will ask you for your name, gender, how many beers you have had and how many hours it has been since your last beer. Run the command bac to answer the previous questions and know if you should drive or not.".yellow);
+console.log("This is an application which will help determine your blood alchol content (BAC). We will ask you for your name, gender, how many Medallas you have had and how many hours it has been since your last beer. Run the command \'bac\' to answer the previous questions, and given your BAC we will let you know if you should drive or not.".yellow);
