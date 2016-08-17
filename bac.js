5a2f2b9d959a5012901eb3f7e5e5c9a6a3c53825
monicafp <monikfornaris@gmail.com>
2016-08-16 20:32:06 -0400

    Edit bac function

--- bac.js
+++ bac.js
@@ -19,9 +19,12 @@ if (gender === 1) {
 
 var BAC = (cansOfBeer * 10 * 5.14 / weight * gender) - 0.015 * hoursSinceLastDrink;
 
-
-if (BAC < 0.08) {
-  console.log(name.green + ", your blood alcohol content is ".green + BAC.toPrecision(2).green + "%\nYou are ok to drive. Be safe!".green);
-} else {
-  console.log(name.red + ", your blood alcohol content is ".red + BAC.toPrecision(2).red + "%\nDo not drive!".red);
+function bac() {
+  if (BAC < 0.08) {
+    return name.green + ", your blood alcohol content is ".green + BAC.toPrecision(2).green + "%\nYou are ok to drive. Be safe!".green;
+  } else {
+    return name.red + ", your blood alcohol content is ".red + BAC.toPrecision(2).red + "%\nDo not drive!".red;
+  }
 }
+
+console.log(bac());
